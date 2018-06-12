354b0d9502cd9381fcb518d22177dd979620aaed
talyag <talyag@wix.com>
2018-06-12 14:31:09 +0300

    add test to guard external network blocking

--- LocalHostResolvingTest.java
+++ LocalHostResolvingTest.java
@@ -1,40 +1,61 @@
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import java.net.*;
+
 import java.io.IOException;
+import java.net.*;
 
 @RunWith(JUnit4.class)
 public class LocalHostResolvingTest {
 
-	@Test
-	public void shouldBindToLocalHost() throws InterruptedException, IOException {
-		Socket s = new Socket();
-    		InetAddress host = InetAddress.getLocalHost();
-    		InetSocketAddress address = new InetSocketAddress(host, 0);
-		System.out.println("Address - " + address);
-    		s.bind(address);
-    		s.close();
-	}
+    @Test
+    public void shouldBindToInetAddressGetLocalHost() throws IOException {
+        Socket s = new Socket();
+        InetAddress host = InetAddress.getLocalHost();
+        InetSocketAddress address = new InetSocketAddress(host, 0);
+        System.out.println("InetAddress.getLocalHost - Address - " + address);
+        s.bind(address);
+        s.close();
+        System.out.println("success");
+    }
 
-	@Test
-        public void shouldBindTolocalhost() throws InterruptedException, IOException {
-       		Socket s = new Socket();
-                String host = "localhost";
-                InetSocketAddress address = new InetSocketAddress(host, 0);
-                System.out.println("Address - " + address);
-                s.bind(address);
-                s.close();
+    @Test
+    public void shouldBindTolocalhost() throws IOException {
+        Socket s = new Socket();
+        String host = "localhost";
+        InetSocketAddress address = new InetSocketAddress(host, 0);
+        System.out.println("'localhost' - Address - " + address);
+        s.bind(address);
+        s.close();
+        System.out.println("success");
+    }
+
+    @Test(expected = BindException.class)
+    public void shouldNotBindToNonLoopbackAddress() throws IOException {
+        InetAddress[] allByName = InetAddress.getAllByName(InetAddress.getLocalHost().getCanonicalHostName());
+        String externalAddress = "";
+        for (InetAddress address : allByName) {
+            if (!address.isLoopbackAddress())
+                externalAddress = address.getHostAddress();
         }
 
-	@Test
-        public void shouldBindToAnyLocalHost() throws InterruptedException, IOException {
-                Socket s = new Socket();
-                String host = "0.0.0.0";
-                InetSocketAddress address = new InetSocketAddress(host, 0);
-                System.out.println("Address - " + address);
-                s.bind(address);
-                s.close();
-        }
+        Socket s = new Socket();
+        InetSocketAddress address = new InetSocketAddress(externalAddress, 0);
+        System.out.println("'externalAddress' - Address - " + address);
+        s.bind(address);
+        s.close();
+        System.out.println("InetAddress");
+    }
+
+    @Test
+    public void shouldBindToAnyLocalHost() throws IOException {
+        Socket s = new Socket();
+        String host = "0.0.0.0";
+        InetSocketAddress address = new InetSocketAddress(host, 0);
+        System.out.println("'0.0.0.0' - Address - " + address);
+        s.bind(address);
+        s.close();
+        System.out.println("InetAddress");
+    }
 
 }
